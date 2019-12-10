f17663451748ff9aba7eedf0e6cca07b6965ce97
pslyp <nonny_038@hotmail.com>
2019-12-10 16:31:59 +0700

    fixed uri mongodb

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-var uri = 'mongodb://localhost:27017/local';
-// const uri = 'mongodb://heroku_54cbqx7k:vi7bjjbn8ln21isu6f8uafff0k@ds121834.mlab.com:21834/heroku_54cbqx7k'
+// var uri = 'mongodb://localhost:27017/local';
+const uri = 'mongodb://heroku_k0ndrzwt:1tm03bvd5v5fpc9elqhr1n2c3n@ds353378.mlab.com:53378/heroku_k0ndrzwt'
 
 module.exports = function() {
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
