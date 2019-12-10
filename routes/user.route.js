5d0d5b08694550378b78639b43d9b856f9afed0a
pslyp <nonny_038@hotmail.com>
2019-12-10 16:09:56 +0700

    add function get user by id

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,13 @@
 module.exports = function(app) {
     const user = require('../controllers/api/user.controller')
 
-    app.route('/api/user')
-        .get(user.findAll)
-        .post(user.create);
+    const BASE_URL = '/api/v1';
 
+    app.route(BASE_URL + '/user')
+        .get(user.getAllUser)
+        .post(user.create);
+    app.route(BASE_URL + '/user/:id')
+        .get(user.getUserById);
+    
+    app.param('id', user.parId);
 }
