5f67cf03b2953a7c605292c90edd5589bd1c85c4
pslyp <nonny_038@hotmail.com>
2019-12-10 16:09:56 +0700

    add function get user by id

--- controllers/api/user.controller.js
+++ controllers/api/user.controller.js
@@ -19,9 +19,9 @@ exports.create = function(req, res) {
     });
 };
 
-exports.findAll = function(req, res) {
+exports.getAllUser = function(req, res) {
 
-    User.find({}, { '_id': 0, 'username': 1, 'email': 1, "devices": 1 }, function(err, user) {
+    User.find({}, { '_id': 0, 'id': 1, 'username': 1, 'email': 1, "devices": 1 }, function(err, user) {
         if(err) {
             console.log('Fail');
 
@@ -32,4 +32,36 @@ exports.findAll = function(req, res) {
         }
     });
 
+};
+
+exports.getUserById = function(req, res) {
+
+    User.findOne({ id: req.user.id }, { '_id': 0, 'username': 1, 'email': 1, 'devices': 1 }, function(err, user) {
+        if(err) {
+            console.log('Find One By ID Fail');
+
+            res.status(500);
+        } else {
+            res.json(user);
+        }
+    });
+
+};
+
+exports.parId = function(req, res, next, id) {
+    User.findOne({ id: id }, function(err, user) {
+        if(err) {
+            return next(err);
+        } else {
+            if(user != null) {
+                req.user = user;
+                next();
+            } else {
+                console.log('Find not found for id user');
+
+                res.status(204);
+                res.json({ message: 'Find not found for id user' });
+            }
+        }
+    });
 };
