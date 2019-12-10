34cde3347cbc806d8d75c9f828775b5538b9e879
pslyp <nonny_038@hotmail.com>
2019-12-10 16:15:14 +0700

    fixed config mongoose connect

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -3,7 +3,7 @@ var uri = 'mongodb://localhost:27017/local';
 // const uri = 'mongodb://heroku_54cbqx7k:vi7bjjbn8ln21isu6f8uafff0k@ds121834.mlab.com:21834/heroku_54cbqx7k'
 
 module.exports = function() {
-    mongoose.connect(uri, {useNewUrlParser: true});
+    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
     require('../models/user.model');
     require('../models/device.model');
