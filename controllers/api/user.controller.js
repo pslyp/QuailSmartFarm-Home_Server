b11e806feb665983208a660de8ee9d1d21f557ca
pslyp <nonny_038@hotmail.com>
2020-02-28 14:29:28 +0700

    fixed response

--- controllers/api/user.controller.js
+++ controllers/api/user.controller.js
@@ -6,9 +6,10 @@ exports.create = function(req, res) {
 
     user.save(function(err) {
         if(err) {
-            res.status(500).send(err);
+            res.status(500).json({ status: 500 });
+            throw err;
         } else {
-            res.status(201).end();
+            res.status(201).json({ status: 201 });
         }
     });
 };
@@ -17,10 +18,10 @@ exports.getAll = function(req, res) {
 
     User.find({}, { '_id': 0, 'id': 1, 'username': 1, 'email': 1 }, function(err, user) {
         if(err) {
-            res.status(500).end();
+            res.status(500).json({ status: 500 });
             throw err;
         } else {
-            res.json(user)
+            res.json(user);
         }
     }).populate('devices');
 };
@@ -30,7 +31,7 @@ exports.getById = function(req, res) {
 
     User.findOne({ id: _id }, { _id: 0, username: 1, email: 1 }, function(err, user) {
         if(err) {
-            res.status(500).end();
+            res.status(500).json({ status: 500 });
             throw err;
         } else {
             res.json(user);
@@ -44,18 +45,20 @@ exports.login = function(req, res) {
 
     User.findOne({ email: _email }, function(err, user) {
         if(err) {
-            res.status(500).end();
+            res.status(500).json({ status: 500 });
+            throw err;
         } else {
             if(user == null) {
-                res.status(401).end();
+                res.status(401).json({ status: 401 });
             } else if(_password != user.password) {
                 // res.json({ message: "Success" });
-                res.status(416).end();
+                res.status(416).json({ status: 416 });
             } else {
                 const payload = { id: user.id, name: user.username }
                 const token = verify.genToken(payload)
 
                 res.json({ 
+                    status: 200,
                     id: user.id,
                     username: user.username,
                     token: token 
@@ -71,7 +74,7 @@ exports.parId = function(req, res, next, _id) {
         if(err) {
             return next(err);
         } else if(user == null) {
-            res.status(204).end();
+            res.status(204).json({ status: 204 });
         } else {
             req.user = user;
             next();
